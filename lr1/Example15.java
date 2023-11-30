b0eac429c6b88a166df321bb13e092c8b3be64e6
Yuliya932 <150506401+Yuliya932@users.noreply.github.com>
2023-12-01 00:24:19 +0500

    Add files via upload

---
+++ lr1/Example15.java
@@ -0,0 +1,25 @@
+/*10. Напишите программу, в которой Пользователь вводит два числа,
+а программой вычисляется и отображается сумма и разность этих чисел.
+ */
+
+package lr1;
+import java.util.Scanner;
+
+public class Example15 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        float firstNumber = in.nextFloat();
+
+        System.out.println("Введите второе число: ");
+        float secNumber = in.nextFloat();
+
+        float Sum = firstNumber + secNumber;
+        float Diff = firstNumber - secNumber;
+
+        System.out.println("Сумма: " + Sum);
+        System.out.println("Разность: " + Diff);
+        in.close();
+    }
+}
---
+++ lr1/Example7.java
@@ -0,0 +1,24 @@
+/*2. Напишите программу, в которой Пользователь вводит имя и возраст.
+Программа отображает сообщение об имени и возрасте пользователя.*/
+
+package lr1;
+import java.util.Scanner;
+
+public class Example7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите имя: ");
+        String name = in.nextLine();
+
+        System.out.println("Введите возраст: ");
+        int age = in.nextInt();
+
+
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+
+        in.close();
+    }
+}
